0bda48fbe1d4af0a996d1ece27a669719cab3345
z9877969 <z9877969@ukr.net>
2022-06-14 00:42:21 +0300

    adds spread, rest, destructarisation, examples

---
+++ js/main.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/* 
+  Операція spread як заміна методів slice та concat
+*/
+
+// Копія масива
+
+// Об'єднання масивів
+
+// Розпилення масива в функцію
+
+/* 
+  Операція spread як заміна Object.assign({}, obj)
+*/
+
+// const user = {
+//   name: "Bob",
+//   email: "bob@mail.com",
+//   isActive: false,
+// };
+
+/* 
+  Розпилення складних типів.
+  lodash.cloneDeep
+
+*/
+// const user = {
+//   name: "Bob",
+//   email: "bob@mail.com",
+//   isActive: false,
+//   somArr: [1,2,3]
+// };
+
+
+/* 
+  Операція rest
+*/
+// Напишіть ф-ю яка буде додавати довільну к-сть аргументів
