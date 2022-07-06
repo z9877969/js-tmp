d34c8c26729a5c245ed2cebb137f52c2830ec228
z9877969 <z9877969@ukr.net>
2022-07-07 00:19:19 +0300

    adds base code to js

--- src/js/jsonplaceholder-api.js
+++ src/js/jsonplaceholder-api.js
@@ -1,2 +1,29 @@
 'use strict';
 
+export class JsonPlaceholderApi {
+  #BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+  constructor() {
+    this.page = 1;
+    this.total_pages = 100 / 10;
+  }
+
+  fetchPosts() {
+    const searchParams = new URLSearchParams({
+      _limit: 10,
+      _page: this.page,
+    });
+
+    return fetch(`${this.#BASE_URL}/posts?${searchParams}`).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+}
--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -1,1 +1,53 @@
 'use strict';
+
+export class UnsplashApi {
+  #BASE_URL = 'https://api.unsplash.com';
+  #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+
+  constructor() {
+    this.query = null;
+    this.page = 1;
+  }
+
+  fetchPhotos() {
+    const searchParams = new URLSearchParams({
+      query: this.query,
+      page: this.page,
+      per_page: 12,
+      orientation: 'portrait',
+      client_id: this.#API_KEY,
+    });
+
+    return fetch(`${this.#BASE_URL}/search/photos?${searchParams}`).then(
+      response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+
+        return response.json();
+      }
+    );
+  }
+
+  fetchRandomPhotos() {
+    const searchParams = new URLSearchParams({
+      count: 12,
+      orientation: 'portrait',
+      client_id: this.#API_KEY,
+    });
+
+    return fetch(`${this.#BASE_URL}/photos/random?${searchParams}`).then(
+      response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+
+        return response.json();
+      }
+    );
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+}
