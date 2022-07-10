d3ae98d9830c7141abfd94fd0f1316ec18b18bf8
z9877969 <z9877969@ukr.net>
2022-07-11 00:02:26 +0300

    adds crud, axios

---
+++ src/js/get.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/*
+ * Method (GET).
+ */
+
+// const BASE_URL = 'http://localhost:3000';
---
+++ src/js/put-patch.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/*
+ * Method(PUT/PATCH). Headers ("Content-Type": "application/json"). Body.
+ */
+
+// const BASE_URL = 'http://localhost:3000';
